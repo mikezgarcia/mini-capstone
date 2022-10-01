b5f29ca7fcf3f377c3a5e432a469ac21fd266db6
Michael Garcia <michaelgarcia101@gmail.com>
2022-10-01 13:29:36 +0800

    Initial Commit - Setup Done

---
+++ src/main/java/mikezgarcia/minicapstone/MiniCapstoneApplication.java
@@ -0,0 +1,13 @@
+package mikezgarcia.minicapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mikezgarcia/minicapstone/MiniCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package mikezgarcia.minicapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
